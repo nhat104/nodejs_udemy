b4ab5974ba75c41f7491da78a19435ecb2f6e853
Mai Minh Nhat <minhnhat1042001@gmail.com>
2022-06-26 10:01:34 +0700

    template engine pug, ejs, handlebars

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+// const hbs = require('express-handlebars');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// app.engine('hbs', hbs.engine());
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use(adminData.router);
 app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).render('404', { docTitle: 'Page Not Found' });
+});
 
 app.listen(8080);
--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,20 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product'));
+  res.render('add-product', {
+    docTitle: 'Add Product',
+    path: '/add-product',
+    activeAddProduct: true,
+  });
 });
 
-router.post('/product', (req, res, next) => {
-  console.log('body', req.body);
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports = { router, products };
